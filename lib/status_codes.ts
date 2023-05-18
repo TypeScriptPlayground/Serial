b77f3a7f0a25b94c101dc77753a9d2ae84b15c60
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-05-18 19:31:15 +0200

    Create status_codes.ts

---
+++ lib/status_codes.ts
@@ -0,0 +1,12 @@
+export const enum statusCodes {
+    SUCCESS = 0,
+    CLOSE_HANDLE_ERROR = -1,
+    INVALID_HANDLE_ERROR = -2,
+    READ_ERROR = -3,
+    WRITE_ERROR = -4,
+    GET_PROPERTY_ERROR = -5,
+    SET_PROPERTY_ERROR = -6,
+    SET_TIMEOUT_ERROR = -7,
+    BUFFER_ERROR = -8,
+    NOT_FOUND_ERROR = -9
+}
