cd3fea9f193765e013e75a144ed35c4f6217ba9f
MrKleeblatt <mrkleeblatt@gmail.com>
2023-05-21 22:28:57 +0200

    Better find method
    This method does not implicitly declare statusCodes as `any`

--- lib/errors/error_code.ts
+++ lib/errors/error_code.ts
@@ -2,7 +2,7 @@ import { statusCodes } from "../constants/status_codes.ts";
 
 export class ErrorCode extends Error {
     constructor(code : number) {
-        const errorCodeKey = Object.keys(statusCodes).find(key => statusCodes[key] === code);
+        const errorCodeKey = Object.entries(statusCodes).find(([_,value]) => value === code)?.[0];
         super(`An error has occurred. Error code: ${errorCodeKey} (${code})`);
     }
 }
